75d5d57d41109a5dbb3b0ced9b797c6361491f78
Renato <renato.amazing.it@gmail.com>
2024-08-24 02:16:40 -0500

    add runnerTest

--- src/test/java/definitions/loginDefinition.java
+++ src/test/java/definitions/loginDefinition.java
@@ -1,16 +1,27 @@
 package definitions;
 
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class loginDefinition {
-    public loginDefinition(){
+    public static WebDriver driver;
+
+    public loginDefinition() {
 
     }
+
     @Given("el usuario está en la página de login")
     public void elUsuarioEstaEnLaPaginaDeLogin() {
 
         hooks.driver.get("https://www.saucedemo.com/v1/index.html");
         System.out.println("Hola");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Espere 10 segundos");
     }
 }
